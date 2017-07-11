aa53dc3afea789b14d4bea110bd7de997db235d6
AVIVEKA <alain.vivekananthan@hotmail.fr>
2017-07-11 21:48:00 +0200

    commit

---
+++ ProjetFinale/ProjetFinale.ino
@@ -0,0 +1,129 @@
+
+#include <Servo.h> 
+#define PIR 12
+
+int etat = 0;
+int calibrationTime = 10;
+
+/*Ultrason + LED RGB*/
+const byte TRIGGER_PIN = 2; // Broche TRIGGER
+const byte ECHO_PIN = 3;    // Broche ECHO
+int redPin = 11; //PIN LED Rouge
+int greenPin = 10; //PIN LED Verte
+int bluePin = 9; // PIN LED Bleue
+
+/* Constantes pour le timeout */
+const unsigned long MEASURE_TIMEOUT = 25000UL; // 25ms = ~8m à 340m/s
+
+
+const float SOUND_SPEED = 340.0 / 1000; // Vitesse du son dans l'air en mm/us 
+ 
+Servo myservo;  // Créer un objet servo pour contrôler               
+int pos = 0;    // Variable pour stocker la position du servo
+ 
+void setup() 
+{ 
+
+  Serial.begin(115200);   // Initialise le port série
+   
+  /* Initialise les broches */
+  pinMode(TRIGGER_PIN, OUTPUT);
+  digitalWrite(TRIGGER_PIN, LOW); // La broche TRIGGER doit être à LOW au repos
+  pinMode(ECHO_PIN, INPUT); //PIN en ENTREE
+  pinMode(redPin, OUTPUT); //PIN Led en SORTIE
+  pinMode(greenPin, OUTPUT); //PIN Led en SORTIE
+  pinMode(bluePin, OUTPUT); //PIN Led en SORTIE
+  
+   for(int i = 0; i < calibrationTime; i++){
+    Serial.print(".");
+    delay(400);
+  }
+  Serial.println();
+  Serial.println("Capteur Calibre");
+  
+  Serial.begin(9600); //Moniteur série
+  pinMode(PIR, INPUT); // Broche 12 configurée en entrée
+  myservo.attach(9);  
+} 
+ 
+ 
+void loop() 
+{ 
+
+   // Lance une mesure de distance en envoyant une impulsion HIGH de 10µs sur la broche TRIGGER 
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  
+  long measure = pulseIn(ECHO_PIN, HIGH, MEASURE_TIMEOUT); // Mesure le temps entre l'envoi de l'impulsion ultrasonique et son écho (si il existe)
+   
+ 
+  float distance_mm = measure / 2.0 * SOUND_SPEED;  // Calcul la distance à partir du temps mesuré 
+   
+  /* Affiche les résultats en mm, cm et m */
+  Serial.print(F("Distance: "));
+  Serial.print(distance_mm);
+  Serial.print(F("mm ("));
+  Serial.print(distance_mm / 10.0, 2);
+  Serial.println(F("cm) "));
+
+  if (distance_mm >= 174){
+    Serial.println("Poubelle remplie a 0%");
+      setColor(0, 255, 0);  // LED Verte
+  }
+
+  if (distance_mm < 173 && distance_mm > 131 ){
+    Serial.println("Poubelle remplie a 25%");
+    setColor(255, 255, 0);  // LED Jaune
+    
+  }
+
+  if (distance_mm < 130 && distance_mm > 88){
+    Serial.println("Poubelle remplie a 50%");
+    setColor(255, 255, 0);  // LED Jaune
+  }
+
+  if (distance_mm < 120 && distance_mm > 90) {
+    Serial.println("Poubelle remplie a 75%");
+    setColor(255, 0, 0);  // LED Rouge
+  }
+  if (distance_mm < 85) {
+    Serial.println("Poubelle remplie a 100%");
+    setColor(255, 0, 0);  // LED Rouge
+  } 
+  delay(1000);  //Délai d'attente pour éviter d'afficher trop de résultats à la seconde
+
+  etat = digitalRead(PIR); // Lit l'état de la broche 12
+  Serial.print(etat); //Affiche l'état dans le moniteur série
+  delay(300); // Délai pour ne pas écrire trop vite dans le moniteur
+
+  if (etat == 1) {
+  for(pos = 0; pos < 180; pos += 1)  // Servomoteur de 0 à 180 degrés 
+  {                                  // 
+    myservo.write(pos);              //  
+                             
+  } 
+    
+  }
+  if (etat == 0) {
+    for(pos = 180; pos>=1; pos-=1)     // Servomoteur de 180 à 0 degrés
+  {                                
+          myservo.write(pos);   // Dit au servo d'aller à la position "pos" 
+                           
+  } 
+  }
+   
+} 
+
+/* Initialisation de la LED */
+void setColor(int red, int green, int blue)
+{
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
+}
